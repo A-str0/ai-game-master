b95f4cbdca5840847e00a8a5c4990c1c0f74c7dc
A-str0 <il.eroxin@gmail.com>
2026-03-25 19:36:14 +0300

    feat/context-objects: create basic repository trait

---
+++ core/application/src/ports/context_object_repository.rs
@@ -0,0 +1,10 @@
+use domain::{aggregates::ContextObject, value_objects::ContextObjectId};
+
+use crate::ports::RepoResult;
+
+#[async_trait::async_trait]
+pub trait ContextObjectRepository: Send + Sync {
+    async fn create(&self, context_object: &ContextObject) -> RepoResult<()>;
+    async fn get_by_id(&self, id: &ContextObjectId) -> RepoResult<ContextObject>;
+    async fn update(&self, object: &ContextObject) -> RepoResult<()>;
+}
--- core/application/src/ports/mod.rs
+++ core/application/src/ports/mod.rs
@@ -1,5 +1,6 @@
 mod agent_orchestrator;
 mod clock;
+mod context_object_repository;
 mod current_user;
 mod game_session_repository;
 mod id_generator;
@@ -9,6 +10,7 @@ use thiserror::Error;
 
 pub use agent_orchestrator::*;
 pub use clock::*;
+pub use context_object_repository::*;
 pub use current_user::*;
 pub use game_session_repository::*;
 pub use id_generator::*;
